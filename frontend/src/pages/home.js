167ae86d9d98ffbd3603ad6de0ae86dc5a587f20
chirag bansal <chirageng.stu22@gmail.com>
2024-03-22 15:03:15 +0530

    sender message showed

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -5,9 +5,10 @@ import Cookies from 'js-cookie';
 
 function Home() {
     const [socketid, setSocketid] = useState("");
+    const [sender, setSender] = useState(["you"]);
 
-    async function connectsocket() {
-        if (socketid == "") {
+    async function connectSocket() {
+        if (!socketid) { // Check if socket is not already connected
             try {
                 const socket = await io('http://localhost:80', {
                     transports: ['websocket'],
@@ -20,20 +21,20 @@ function Home() {
                 });
 
                 socket.on("message", (data) => {
-                    console.log(data);
-                })
+                  
+                    console.log(sender, 'res'); // Log the actual value of res
+                   
+                        setSender(prevSender => [...prevSender, data.sender]); // Add sender to the list
+                   
+                });
+                
             } catch (error) {
                 console.error('Error connecting to socket:', error);
             }
-
-        }
-
-        else {
-
         }
     }
 
-    async function setsocket() {
+    async function setSocket() {
         try {
             const response = await axios.post('http://localhost:80/setsocketid', {
                 socketid: socketid
@@ -44,24 +45,40 @@ function Home() {
             });
 
             console.log(response.data);
+            // Load sender list from cookies after setting socket
+            const savedSender = Cookies.get("sender");
+            if (savedSender) {
+                setSender(JSON.parse(savedSender));
+            }
         } catch (error) {
             console.error('Error setting socket:', error);
         }
     }
 
     useEffect(() => {
-        connectsocket();
+        connectSocket();
     }, []);
 
     useEffect(() => {
         if (socketid) {
-            setsocket();
+            setSocket();
         }
     }, [socketid]);
 
+    useEffect(() => {
+        // Save sender list to cookies whenever it changes
+        Cookies.set("sender", sender);
+    }, [sender]);
+
     return (
         <div className="App">
-            home
+            {
+                sender.map((senderName, index) => (
+                    <div key={index}>
+                         {senderName}
+                    </div>
+                ))
+            }
         </div>
     );
 }
