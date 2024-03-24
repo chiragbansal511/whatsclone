794d192cd292372c9af43f61ee53afd4e5c6a313
chirag bansal <chirageng.stu22@gmail.com>
2024-03-24 18:54:02 +0530

    sending message set

--- frontend/src/pages/sendmessage.js
+++ frontend/src/pages/sendmessage.js
@@ -2,19 +2,58 @@ import { useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useState, useEffect } from "react";
 import socket from "../socket";
+import axios from "axios";
+import "./index.css";
 
 export default function Sendmessage() {
     const location = useLocation();
     const sender = location.state.data;
     const [message, setMessage] = useState([]);
+    const [sendmessage, setSendmessage] = useState("");
+
+    async function handlesubmit() {
+        try {
+            const response = await axios.post('http://localhost:80/message', {
+
+                message: sendmessage,
+                receiver: sender
+            }, {
+                headers: {
+                    Authorization: "Bearer " + Cookies.get("accessToken")
+                }
+            });
+
+            const message = {
+                type: "receiver",
+                sender: sender,
+                message: sendmessage,
+            }
+
+            let prevmessage = Cookies.get(message.sender);
+            if (prevmessage != undefined) {
+                prevmessage = JSON.parse(prevmessage);
+                prevmessage.push(message);
+            }
+
+            else {
+                prevmessage = [message];
+            }
+            Cookies.set(message.sender, JSON.stringify(prevmessage));
+            setMessage(prevMessages => [...prevMessages, { message: sendmessage, type: "receiver" }]);
+            setSendmessage("");
+            console.log(response);
+        } catch (error) {
+
+        }
+    }
 
     useEffect(() => {
         let data = Cookies.get(sender);
-        if(data != undefined)
-        {   data = JSON.parse(data);
+        if (data != undefined) {
+            data = JSON.parse(data);
             let datamessage = [];
             data.forEach(element => {
-                datamessage.push(element.message);
+                datamessage.push({ message: element.message, type: element.type });
             });
             setMessage(datamessage);
         }
@@ -25,14 +64,13 @@ export default function Sendmessage() {
         const messageHandler = (data) => {
 
             const message = {
-                type : "sender",
-                sender : data.sender,
-                message : data.message,
+                type: "sender",
+                sender: data.sender,
+                message: data.message,
             }
 
             let prevmessage = Cookies.get(message.sender);
-            if(prevmessage != undefined)
-            {
+            if (prevmessage != undefined) {
                 prevmessage = JSON.parse(prevmessage);
                 prevmessage.push(message);
             }
@@ -40,7 +78,7 @@ export default function Sendmessage() {
             else {
                 prevmessage = [message];
             }
-            Cookies.set(message.sender , JSON.stringify(prevmessage));
+            Cookies.set(message.sender, JSON.stringify(prevmessage));
 
             let list = Cookies.get("sender");
             if (list != null) {
@@ -52,13 +90,13 @@ export default function Sendmessage() {
             }
 
             else {
-                Cookies.set("sender" , [data.sender]);
+                Cookies.set("sender", [data.sender]);
             }
 
 
             if (data.sender === sender) {
 
-                setMessage(prevMessages => [...prevMessages, data.message]);
+                setMessage(prevMessages => [...prevMessages, { message: data.message, type: "sender" }]);
             }
         };
 
@@ -71,14 +109,18 @@ export default function Sendmessage() {
 
 
     return (
-        <div>
+        <div className="container">
             {
-                message.map((e , index)=>(
-                    <div key={index} >
-                        {e}
+                message.map((e, index) => (
+                    <div key={index} className={e.type}>
+                        <div>{e.message}</div>
                     </div>
                 ))
             }
+
+            <div>send message</div>
+            <input type="text" value={sendmessage} onChange={(e) => setSendmessage(e.target.value)} />
+            <button onClick={handlesubmit}>send</button>
         </div>
     )
 }
