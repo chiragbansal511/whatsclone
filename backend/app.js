ce3cba1520168c21e10816f4a5c6c441242e3caf
chirag bansal <chirageng.stu22@gmail.com>
2024-04-07 21:17:39 +0530

    mongodb atlas setup

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const io = socketIo(server);
-url = "mongodb://0.0.0.0:27017/";
+url = "mongodb+srv://chiragengstu22:12345@whatsapp.avhjkr0.mongodb.net/?retryWrites=true&w=majority&appName=whatsapp";
 const client = new MongoClient(url);
 const secretKey = 'your_secret_key';
 const dbName = 'whatsapp';
